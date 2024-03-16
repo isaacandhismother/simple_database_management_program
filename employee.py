a951ba45977705532f945c5e2e5481a095c3b109
isaacandhismother <vitaliboshik40@gmail.com>
2024-03-16 15:11:10 +0400

    Initial commit

---
+++ employee.py
@@ -0,0 +1,69 @@
+from db import c, conn
+
+
+"""
+PK - Primary Key
+"""
+
+
+class Employee(object):
+    def __init__(self, name, surname, age, pk=None):
+        self.id = pk
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def __repr__(self):
+        return "<Employee {} {}, {} years old>".format(self.name, self.surname, self.age)
+
+    def __eq__(self, other):
+        return self.age == other.age
+
+    @classmethod
+    def get(cls, pk):
+        result = c.execute("SELECT * FROM employee WHERE id = ?", (pk,))
+        values = result.fetchone()
+        if values is None:
+            return None
+        employee = Employee(values["name"], values["surname"], values["age"], values["id"])
+        return employee
+
+    @classmethod
+    def get_list(cls, **kwargs):
+        conditions = []
+        values = ()
+
+        for key, value in kwargs.items():
+            conditions.append(f"{key} = ?")
+            values += (value,)
+
+        base = "SELECT * FROM employee"
+
+        if conditions:
+            base += " WHERE " + " AND ".join(conditions)
+
+        result = c.execute(base, values)
+        employees = [cls(row["name"], row["surname"], row["age"], row["id"]) for row in result.fetchall()]
+
+        return employees
+
+    def update(self):
+        c.execute("UPDATE employee SET name = ?, surname = ?, age = ? WHERE id = ?",
+                  (self.name, self.surname, self.age, self.id))
+
+    def create(self):
+        c.execute("INSERT INTO employee (name, surname, age) VALUES (?, ?, ?)", (self.name, self.surname, self.age))
+        self.id = c.lastrowid
+
+    def delete(self):
+        if self.id is None:
+            return
+        c.execute("DELETE FROM employee WHERE id = ?", (self.id,))
+        self.id = None
+
+    def save(self):
+        if self.id is not None:
+            self.update()
+        else:
+            self.create()
+        return self
---
+++ main.py
@@ -0,0 +1,28 @@
+from employee import Employee
+from db import conn
+
+
+first_user = Employee.get(1)
+second_user = Employee.get(2)
+if first_user is None:
+    first_user = Employee("name", "surname", "age")
+    first_user.save()
+
+if second_user is None:
+    second_user = Employee("Sherlock", "Holmes", "35")
+    second_user.save()
+
+first_user.name = "Tornike"
+first_user.surname = "Namchishvili"
+first_user.age = "20"
+first_user.save()
+
+print(Employee.get_list(name="Tornike"))
+print(Employee.get_list(name="Sherlock"))
+
+second_user.delete()
+
+print(Employee.get_list(name="Sherlock"))
+
+conn.commit()
+conn.close()
